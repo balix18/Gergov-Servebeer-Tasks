351dd88de9236623676d689ba3b98093441ba498
Balazs Oczot <balix18@gmail.com>
2018-08-21 21:31:34 +0200

    01 - Basics elkészítése

---
+++ Gergov-Servebeer-Tasks.01.Basics/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace Gergov_Servebeer_Tasks._01.Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 12, 2, 46, 9, 13, 32, 1, 8 };
+
+            SolveTask1(array);
+            SolveTask2(array);
+            SolveTask3(array);
+            SolveTask4(array);
+            SolveTask5(array);
+        }
+
+        static void SolveTask1(int[] array)
+        {
+            Console.WriteLine("Írasd ki a képernyõre a tömb elsõ 5 elemét!");
+
+            foreach (var element in array.Take(5))
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void SolveTask2(int[] array)
+        {
+            Console.WriteLine("Írasd ki a tömb elemeit fordított sorrendben!");
+
+            foreach (var element in array.Reverse())
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void SolveTask3(int[] array)
+        {
+            Console.WriteLine("Írasd ki a képernyõre a tömb elsõ, majd minden 3. elemét!");
+
+            foreach (var element in array.Where((item, index) => index % 3 == 0))
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void SolveTask4(int[] array)
+        {
+            Console.WriteLine("Írasd ki a tömb páros elemeit!");
+
+            foreach (var element in array.Where(item => item % 2 == 0))
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        static void SolveTask5(int[] array)
+        {
+            Console.WriteLine("Írasd ki a tömb elsõ n elemét, egészen addig, amíg nem találsz egy 40-nél nagyobbat!");
+
+            foreach (var element in array.TakeWhile(item => item <= 40))
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ Gergov-Servebeer-Tasks.01.Basics/README.md
@@ -0,0 +1,12 @@
+# Gergov-Servebeer-Tasks.01.Basics
+
+Adott az alábbi, egész számokat tartalmazó tömb:
+```csharp
+int[] tomb = { 5, 12, 2, 46, 9, 13, 32, 1, 8 };
+```
+
+1. Írasd ki a képernyõre a tömb elsõ 5 elemét!
+2. Írasd ki a tömb elemeit fordított sorrendben!
+3. Írasd ki a képernyõre a tömb elsõ, majd minden 3. elemét!
+4. Írasd ki a tömb páros elemeit!
+5. Írasd ki a tömb elsõ n elemét, egészen addig, amíg nem találsz egy 40-nél nagyobbat!
